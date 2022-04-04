8b464fdc44e118460fdd8e5fdaeff34eaef9d49a
itzguled <itzguled55337@gmail.com>
2022-04-04 15:00:57 -0500

    handlebars

--- models/index.js
+++ models/index.js
@@ -1,42 +1,40 @@
 // import all models
-const Rate = require('./Rate');
-const User = require('./User');
-const Comment = require('./Comment');
-
+const Rate = require("./Rate");
+const User = require("./User");
+const Comment = require("./Comment");
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: "user_id",
+});
 
-  Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-  });
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
 
-  User.belongsToMany(Comment, {
-    through: Rate,
-    as: 'rated_posts',
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-  });
+User.belongsToMany(Comment, {
+  through: Rate,
+  as: "rated_posts",
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
 
+Rate.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
 
-  Rate.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-  });
+Rate.belongsTo(Comment, {
+  foreignKey: "comment_id",
+  // onDelete: 'SET NULL'
+});
 
-  Rate.belongsTo(Comment, {
-    foreignKey: 'comment_id'
-   // onDelete: 'SET NULL'
-  });
+User.hasMany(Rate, {
+  foreignKey: "user_id",
+});
 
-  User.hasMany(Rate, {
-    foreignKey: 'user_id'
-  });
+Comment.hasMany(Rate, {
+  foreignKey: "comment_id",
+});
 
-  Comment.hasMany(Rate, {
-    foreignKey: 'comment_id'
-  });
-  
-  module.exports = { User, Rate,  Comment };
+module.exports = { User, Rate, Comment };
