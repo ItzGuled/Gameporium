f2d354b99f6d9eea85597c5e4e25f6c7e09df951
Glorzo <thisaraeranga@gmail.com>
2022-04-05 21:07:09 -0500

    3rd party api done

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
-const { games } = require('../data/games.json');
+// const { games } = require('../data/games.json');
 const sequelize = require('../config/connection');
 const { User, Comment, Rate } = require('../models');
-
+const axios = require('axios');
 
 
 router.get('/', (req,res) => {
-    console.log(games);
+    axios.get('https://www.giantbomb.com/api/games/?api_key=f69140845ff4d02155385c0b54aa36d12f51108f&format=json&limit=100&sort=original_release_date:desc&platforms=pc')
+        .then(function(response) {              
+            const games = response.data.results;      
+            res.render('homepage', { games });
+            
+        }).catch(function(error) {
+            console.log(error);
+        })
+    
 })
 
-
+router.get('/:id', (req,res))
 // login
 module.exports = router;
+
+
