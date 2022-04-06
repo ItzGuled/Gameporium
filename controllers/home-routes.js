21613b1c2e0c9aedb04250f8a8794203cd92faa2
Glorzo <thisaraeranga@gmail.com>
2022-04-06 17:26:09 -0500

    catching up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 const { User, Comment, Rate } = require('../models');
 const axios = require('axios');
 
-
+//GET all games on the front-end
 router.get('/', (req,res) => {
     axios.get('https://www.giantbomb.com/api/games/?api_key=f69140845ff4d02155385c0b54aa36d12f51108f&format=json&limit=100&sort=original_release_date:desc&platforms=pc')
         .then(function(response) {              
@@ -17,7 +17,13 @@ router.get('/', (req,res) => {
     
 })
 
-router.get('/:id', (req,res))
+// GET single game
+router.get('/:id', (req,res) => {
+    axios.get('https://www.giantbomb.com/api/game/'+ req.params.guid + '/?api_key=f69140845ff4d02155385c0b54aa36d12f51108f&format=json')
+        .then(function(response) {
+            const game = response.data.results;
+        })
+})
 // login
 module.exports = router;
 
