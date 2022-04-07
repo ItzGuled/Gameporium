1d35e60c7e8fc860b90ba7b2b23fb87f5b061761
Stephanie <stephanieanderson5133@gmail.com>
2022-04-06 19:10:19 -0500

    added comment.js

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -4,17 +4,18 @@ async function commentRateFormHandler(event) {
     //will have to change this to reflect the part of the comment body
     const comment_text = document.querySelector('textarea[name="comment-section"]').value.trim();
     //will have to change this to reflect the part of the rating body
-    const rating = document.querySelector('textarea[name="rating-body"]').value.trim();
+    const rating = document.querySelector('#rating-id').value.trim();
     const game_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-  
-    if (comment_text) {
+
+    if (comment_text || rating) {
       const response = await fetch('/api/commentrates', {
         method: 'POST',
         body: JSON.stringify({
           comment_text,
-          rating
+          rating,
+          game_id
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -29,6 +30,6 @@ async function commentRateFormHandler(event) {
     }
   }
   
-  //
-  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+  //change this to the div for comments
+  document.querySelector('.comment-page').addEventListener('submit', commentRateFormHandler );
   
