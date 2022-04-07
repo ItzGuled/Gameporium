9692de36179ea71739ec46c9b60256ce57dc5cd8
Glorzo <thisaraeranga@gmail.com>
2022-04-06 22:30:09 -0500

    merging with stephanie

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 // const { games } = require('../data/games.json');
 const sequelize = require('../config/connection');
-const { User, Comment, Rate } = require('../models');
+const {  User, Commentrate  } = require('../models');
 const axios = require('axios');
 
 //GET all games on the front-end
@@ -23,14 +23,34 @@ router.get('/login', (req,res) => {
         res.redirect('/');
         return;
     }    
-    res.render('login.handlebars');
+    res.render('login');
 });
 
 // GET single game
 router.get('/:guid', (req,res) => {
     axios.get('https://www.giantbomb.com/api/game/'+ req.params.guid + '/?api_key=' + process.env.API_KEY + '&format=json')
         .then(function(response) {
-            const game = response.data.results;
+            const game = response.data.results
+            Commentrate.findOne({
+                attributes: ['comment_text', 'rating', 'created_at'],
+                where: {
+                    game_id: req.params.guid
+                },
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }).then(function(dbCommentData) {
+                if(!dbPostData) {
+                    res.status(404).json({ message: 'No post found with this id' });
+                    return;
+                }
+
+                // serialize the data
+                const post = dbPostData.get({ plain: true });
+            })
             res.render('single-post', { game });
         }).catch(function(error) {
             console.log(error);
@@ -39,7 +59,7 @@ router.get('/:guid', (req,res) => {
 
 // GET comments and ratings
 router.get('/:id', (req,res) => {
-    
+    Commentrate
 })
 
 module.exports = router;
